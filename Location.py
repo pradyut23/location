4cf8f7400cab09244b237533e30602c5396df2fa
Pradyut Lohani <pradyut.lohani@gmail.com>
2019-03-03 15:46:58 +0530

    location.py
    This program uses a maps API to retrieve the full address, latitude and longitude of the given address (user input) from the JSON of the page. Line no. 32 will print the whole JSON.  Line no. 18 prints the URL to be fed after encoding.

---
+++ Location.py
@@ -0,0 +1,41 @@
+import urllib.request, urllib.parse, urllib.error
+import json
+import ssl
+
+serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+address = input('Enter location: ')
+
+parms = dict()
+parms['address'] = address
+parms['key'] = 42
+url = serviceurl + urllib.parse.urlencode(parms)
+
+print('Retrieving', url)
+uh = urllib.request.urlopen(url,context=ctx)
+data = uh.read().decode()
+print('Retrieved', len(data), 'characters')
+
+try:
+    js = json.loads(data)
+except:
+    js = None
+
+if not js or 'status' not in js or js['status'] != 'OK':
+    print('Failure To Retrieve')
+    print(data)
+
+#print(json.dumps(js, indent=2))
+
+location = id = js["results"][0]["formatted_address"]
+lat = id = js["results"][0]["geometry"]["location"]["lat"]
+long = id = js["results"][0]["geometry"]["location"]["lng"]
+print("")
+print("Full Address with Latitude and Longitude:-")
+print(location)
+print("Latitude:",lat)
+print("Longitude:",long)
